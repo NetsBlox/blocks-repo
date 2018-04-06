750ba8810a43846e2966524daf7a5bf1d8360a74
Hamid Zare <dellydela@gmail.com>
2018-04-06 16:35:42 -0500

    added nb logo

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
+    <img id="logo" src="./assets/logo.png">
     <router-view/>
   </div>
 </template>
@@ -19,5 +19,10 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+#logo {
+  max-width: 100px;
+  max-height: 100px;
 }
 </style>
