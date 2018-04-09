f719c999b64ed3b54747cfca068a48769d0fbe74
Hamid Zare <dellydela@gmail.com>
2018-04-08 21:04:43 -0500

    added a component to display modules

---
+++ src/components/Modules.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="container">
+    <h1>Modules</h1>
+    <p>Available modules in the repo</p>
+    <div class="collection">
+      <a v-for="module in modules" href="#!" class="collection-item">{{ module.name }}</a>
+      </div>
+    <ul>
+    </ul>
+  </div>
+</template>
+
+<script>
+import { modules, loadCol } from '@/firebase';
+export default {
+  name: 'Modules',
+  data() {
+    return {
+      modules: []
+    };
+  },
+  created() {
+    this.fetchModules();
+  },
+  methods: {
+    fetchModules() {
+      loadCol(modules)
+        .then(mods => {
+          console.log(mods);
+          this.modules = mods;
+        })
+    }
+  }
+};
+</script>
+
+<style scoped>
+</style>
