c0c23b8e7cbfd332041690024170927e2cebf7db
Hamid Zare <dellydela@gmail.com>
2018-04-09 16:03:22 -0500

    added new module link to navbar

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -3,8 +3,7 @@
     <div class="nav-wrapper">
       <router-link :to="{name: 'Modules'}" class="brand-logo left">NetsBlox</router-link>
       <ul id="nav-mobile" class="right hide-on-small-only">
-        <li><a href="#">Link 1</a></li>
-        <li><a href="#">Link 2</a></li>
+        <li><router-link :to="{name: 'New'}">Post a Module</router-link></li>
       </ul>
     </div>
   </nav>
