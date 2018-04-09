1355735bdef39b3175317c84719bd263318ac4f3
Hamid Zare <dellydela@gmail.com>
2018-04-08 21:15:50 -0500

    added new pages to router

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,8 @@ import Vue from 'vue';
 import Router from 'vue-router';
 import HelloWorld from '@/components/HelloWorld';
 import New from '@/components/New';
+import Modules from '@/components/Modules';
+import Module from '@/components/Module';
 
 Vue.use(Router);
 
@@ -11,6 +13,17 @@ export default new Router({
       path: '/',
       name: 'HelloWorld',
       component: HelloWorld
+    },
+    {
+      path: '/modules',
+      name: 'Modules',
+      component: Modules
+    },
+    {
+      path: '/module/:id',
+      name: 'Module',
+      component: Module,
+      props: true
     },
     {
       path: '/new',
