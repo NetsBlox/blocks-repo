8620441eef079c04995552148d32d02ec3136e52
Hamid Zare <dellydela@gmail.com>
2018-04-08 20:35:32 -0500

    no space before function

--- .eslintrc.js
+++ .eslintrc.js
@@ -24,6 +24,7 @@ module.exports = {
     // allow async-await
     'generator-star-spacing': 'off',
     'semi': ['warn', 'always'],
+    'space-before-function-paren': ['warn', 'never'],
     // allow debugger during development
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
   }
