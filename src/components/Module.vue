293d2434f53b29947e6b99f05037158100e30811
Hamid Zare <dellydela@gmail.com>
2018-04-08 21:15:21 -0500

    WIP module details component

---
+++ src/components/Module.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="container">
+    <h1>Module {{ id }}</h1>
+    <p>Available modules in the repo</p>
+    <div class="collection">
+      <a v-for="module in modules" href="#!" class="collection-item">{{ module.name }}</a>
+      </div>
+    <ul>
+    </ul>
+  </div>
+</template>
+
+<script>
+import { modules, loadCol } from '@/firebase';
+export default {
+  name: 'Module',
+  data() {
+    return {
+    };
+  },
+  props: ['id'],
+  created() {
+    console.log(this.id);
+  },
+  methods: {
+  }
+};
+</script>
+
+<style scoped>
+</style>
