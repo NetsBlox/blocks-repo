6a981df6e3b95e47974f588840642e8cb956ff4b
Hamid Zare <dellydela@gmail.com>
2018-04-10 16:02:24 -0500

    show the profile link when logged in

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -21,7 +21,11 @@ export default {
   },
   created() {
     firebase.auth().onAuthStateChanged(user => {
-      if (user) this.user = user;
+      if (user) {
+        this.user = user;
+      } else {
+        this.user = {};
+      }
     });
   }
 };
