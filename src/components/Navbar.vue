01e60e56e1cba06121dcdc81913c902c3ba76868
Hamid Zare <dellydela@gmail.com>
2018-04-10 15:31:48 -0500

    show a link to profile if logged in

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,20 +1,28 @@
 <template>
   <nav>
     <div class="nav-wrapper">
-      <router-link :to="{name: 'Modules'}" class="brand-logo left">NetsBlox</router-link>
+      <router-link :to="{name: 'Modules'}" class="brand-logo center">NetsBlox</router-link>
       <ul id="nav-mobile" class="right hide-on-small-only">
         <li><router-link :to="{name: 'New'}">Post a Module</router-link></li>
+        <li v-if="!!user.uid"><router-link :to="{name: 'AuthSuccess'}">My Profile</router-link></li>
       </ul>
     </div>
   </nav>
 </template>
 
 <script>
+import firebase from 'firebase';
 export default {
   name: 'Navbar',
   data() {
     return {
+      user: {}
     };
+  },
+  created() {
+    firebase.auth().onAuthStateChanged(user => {
+      if (user) this.user = user;
+    });
   }
 };
 </script>
