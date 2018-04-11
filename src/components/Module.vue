6d71ea4fdb5b188499055eb08d693c294b7292de
Hamid Zare <dellydela@gmail.com>
2018-04-11 18:47:18 -0500

    added displaying of the images, show not found msg on invalid module page

--- src/components/Module.vue
+++ src/components/Module.vue
@@ -1,9 +1,10 @@
 <template>
   <div class="container">
-    <h2>Module {{ name }}</h2>
-    <p>{{ description }}</p>
+    <h2>Module {{ module.name }}</h2>
+    <p>{{ module.description }}</p>
+    <img class="materialboxed" v-for="img in images" :src="img.path" :key="img.filename" :alt="img.originalname"/>
     <ul>
-      <li v-for="file in files" :key="file.filename">{{ file.originalname }}:
+      <li v-for="file in projectFiles" :key="file.filename">{{ file.originalname }}:
         <a :href="file.path" :filename="file.originalname" class="waves-effect waves-light btn">Download</a>
       </li>
     </ul>
@@ -17,18 +18,31 @@ export default {
   name: 'Module',
   data() {
     return {
-      name: '',
-      description: '',
-      files: []
+      module: {
+        name: '',
+        description: '',
+        files: []
+      }
     };
   },
   props: ['id'],
+  computed: {
+    images() {
+      let images = this.module.files.filter(f => {
+        return f.mimetype.includes('image');
+      });
+      return images;
+    },
+    projectFiles() {
+      return this.module.files.filter(f => {
+        return !f.mimetype.includes('image');
+      });
+    }
+  },
   created() {
     this.fetchDoc(this.id)
       .then(module => {
-        this.name = module.name;
-        this.description = module.description;
-        this.files = module.files;
+        this.module = module;
         console.log('loaded doc', module);
       })
       .catch(alert);
@@ -41,6 +55,7 @@ export default {
           if (doc.exists) {
             return doc.data();
           } else {
+            this.module.name = 'Not found.';
             throw new Error(`document ${id} does not exist.`);
           }
         });
@@ -50,4 +65,7 @@ export default {
 </script>
 
 <style scoped>
+  img {
+    max-height: 50vh;
+  }
 </style>
