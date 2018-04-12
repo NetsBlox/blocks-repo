ad65315d39210315b2785384dfba61043666c08d
Hamid Zare <dellydela@gmail.com>
2018-04-12 10:33:37 -0500

    modified navbar to user usermixins

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -4,29 +4,22 @@
       <router-link :to="{name: 'Home'}" class="brand-logo center">NetsBlox</router-link>
       <ul id="nav-mobile" class="right hide-on-small-only">
         <li><router-link :to="{name: 'New'}">Post a Module</router-link></li>
-        <li v-if="!!user.uid"><router-link :to="{name: 'AuthSuccess'}">My Profile</router-link></li>
+        <li v-if="!!user.email"><router-link :to="{name: 'AuthSuccess'}">My Profile</router-link></li>
       </ul>
     </div>
   </nav>
 </template>
 
 <script>
-import firebase from 'firebase';
+import { userMixin } from '@/mixins';
 export default {
   name: 'Navbar',
+  mixins: [userMixin],
   data() {
     return {
-      user: {}
     };
   },
   created() {
-    firebase.auth().onAuthStateChanged(user => {
-      if (user) {
-        this.user = user;
-      } else {
-        this.user = {};
-      }
-    });
   }
 };
 </script>
