814e59137a5788d86726787a624f74f644dde11c
Hamid Zare <dellydela@gmail.com>
2018-04-23 15:43:06 -0500

    responsive navbar

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,13 +1,22 @@
 <template>
-  <nav>
-    <div class="nav-wrapper orange darken-4">
-      <router-link :to="{name: 'Home'}" class="brand-logo center">NetsBlox</router-link>
-      <ul id="nav-mobile" class="right hide-on-small-only">
-        <li><router-link :to="{name: 'New'}">Post a Module</router-link></li>
-        <li v-if="!!user.email"><router-link :to="{name: 'AuthSuccess'}">My Profile</router-link></li>
-      </ul>
-    </div>
-  </nav>
+  <div>
+    <nav>
+      <div class="nav-wrapper orange darken-4">
+        <router-link :to="{name: 'Home'}" class="brand-logo center">NetsBlox</router-link>
+        <a href="#" data-target="mobile-demo" class="sidenav-trigger">
+          <i class="material-icons">menu</i>
+        </a>
+        <ul id="nav-mobile" class="right hide-on-small-only">
+          <li><router-link :to="{name: 'New'}">Post a Module</router-link></li>
+          <li v-if="!!user.email"><router-link :to="{name: 'AuthSuccess'}">My Profile</router-link></li>
+        </ul>
+      </div>
+    </nav>
+    <ul class="sidenav" id="mobile-demo">
+      <li><router-link :to="{name: 'New'}">Post a Module</router-link></li>
+      <li v-if="!!user.email"><router-link :to="{name: 'AuthSuccess'}">My Profile</router-link></li>
+    </ul>
+  </div>
 </template>
 
 <script>
@@ -19,7 +28,10 @@ export default {
     return {
     };
   },
-  created() {
+  mounted() {
+    /* global M */
+    let elem = document.querySelector('.sidenav');
+    M.Sidenav.init(elem);
   }
 };
 </script>
