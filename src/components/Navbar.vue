63d5f13741728315f92813c7acf7321ad57de459
Hamid Zare <dellydela@gmail.com>
2019-04-17 11:03:49 -0500

    login button

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -8,6 +8,7 @@
         </a>
         <ul id="nav-mobile" class="right hide-on-small-only">
           <li><router-link :to="{name: 'New'}">Post a Module</router-link></li>
+          <li v-if="!user.email"><router-link :to="{name: 'Auth'}">Login</router-link></li>
           <li v-if="!!user.email"><router-link :to="{name: 'AuthSuccess'}">My Profile</router-link></li>
         </ul>
       </div>
